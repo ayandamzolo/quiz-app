117b905fbe8906e0e15c7147da3c0ece0792b6a7
ayandamzolo <135819703+ayandamzolo@users.noreply.github.com>
2025-05-19 20:09:33 +0200

    push code

---
+++ quiz-app/src/services/local_store_service.js
@@ -0,0 +1,42 @@
+import { User } from "../models/user_model.js";
+
+export class LocalStorageService {
+  saveUser(user) {
+    if (user instanceof User) {
+      localStorage.setItem(user.userName, JSON.stringify(user.toString()));
+    }
+  }
+
+  retrieveUser(user) {
+    if (user instanceof User) {
+      return JSON.parse(localStorage.getItem(user.userName));
+    }
+  }
+
+  setTheScore(user, score) {
+    user.score = score;
+    this.saveUser(user);
+  }
+
+  getLeaderboard(category) {
+    const players = [];
+
+    for (let i = 0; i < localStorage.length; i++) {
+      const { userName, category, correctAnswers } = JSON.parse(
+        localStorage.getItem(localStorage.key(i)),
+      );
+
+      players.push({
+        userName: userName,
+        category: category,
+        correctAnswers: correctAnswers,
+      });
+    }
+
+    return players
+      .filter((player) => player.category[0] === category)
+      .sort(function (player1, player2) {
+        return player2.correctAnswers - player1.correctAnswers;
+      });
+  }
+}
